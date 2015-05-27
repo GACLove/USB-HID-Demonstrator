70cb96e1afee6718cd34cc3051d7a355e03cbd09
gaopeng <gaopeng0108@126.com>
2015-05-27 10:41:53 +0800

    Signed-off-by: gaopeng <gaopeng0108@126.com>

---
+++ UsbHidDll/UsbHidDll.cpp
@@ -0,0 +1,4 @@
+// UsbHidDll.cpp : 定义 DLL 应用程序的导出函数。
+//
+#include "stdafx.h"
+#include "UsbHidDll.h"
---
+++ UsbHidDll/UsbHidDll.h
@@ -0,0 +1,49 @@
+// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 USBHIDDLL_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何其他项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// USBHIDDLL_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef USBHIDDLL_EXPORTS
+#define USBHIDDLL_API __declspec(dllexport)
+#else
+#define USBHIDDLL_API __declspec(dllimport)
+#endif
+
+#include "hidsdi++.h"
+//#include "hid.h"
+//#include "setupapi.h"
+//#include "usb100.h"
+
+#define USBHANDLE void* 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+enum USBHIDTYPE
+{
+    T_Report,
+    T_Feature,
+};
+
+
+// 打开设备
+USBHIDDLL_API USBHANDLE USBHIDCreateUsbHid(char* devicePath);
+
+// 配置设备(与写数据相关)
+USBHIDDLL_API void USBHIDSetType(USBHIDTYPE type);
+
+// 读设备
+USBHIDDLL_API int USBHIDReadByte(USBHANDLE handle, BYTE* byte, int len);
+
+// 写设备
+USBHIDDLL_API int USBHIDWriteByte(USBHANDLE handle, BYTE* byte, int len);
+
+// 获取设备一些信息
+USBHIDDLL_API void USBHIDGetDeviceCapabilitiess(USBHANDLE handle, HIDP_CAPS* caps);
+
+#ifdef __cplusplus
+}
+#endif
